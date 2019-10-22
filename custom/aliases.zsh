dff1ffa85b663d9e01985032a6ce06456363fdf8
Daniel Speck <daniel451@mailbox.org>
2019-10-22 14:14:07 +0200

    new aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -7,70 +7,32 @@ alias mount_ws16hdd='sshfs -o idmap=user,umask=$(umask),gid=$(id -g),uid=$(id -u
 alias umount_ws16hdd='fusermount -u ~/mnt/ws16hdd'
 alias mount_ws16ssd='sshfs -o idmap=user,umask=$(umask),gid=$(id -g),uid=$(id -u),user,reconnect,ServerAliveInterval=15 ws16:/srv/ssd ~/mnt/ws16ssd'
 alias umount_ws16ssd='fusermount -u ~/mnt/ws16ssd'
+alias mount_ws16ssd_nvm='sshfs -o idmap=user,umask=$(umask),gid=$(id -g),uid=$(id -u),user,reconnect,ServerAliveInterval=15 ws16:/srv/ssd_nvm ~/mnt/ws16ssd_nvm'
+alias umount_ws16ssd_nvm='fusermount -u ~/mnt/ws16ssd_nvm'
+
+# sshfs - Wolves GPU
+alias mount_wolves_var_storage='sshfs -o idmap=user,umask=$(umask),gid=$(id -g),uid=$(id -u),user,reconnect,ServerAliveInterval=15 wolvesgpu:/var/storage/local/users/dspeck ~/mnt/wolves_var_storage'
+
 # sshfs - hummel
 alias mount_hummel1work='sshfs -o idmap=user,umask=$(umask),gid=$(id -g),uid=$(id -u),user,reconnect,ServerAliveInterval=15 hummel1:/work/intx033 ~/mnt/hummel_work'
 alias mount_hummel2work='sshfs -o idmap=user,umask=$(umask),gid=$(id -g),uid=$(id -u),user,reconnect,ServerAliveInterval=15 hummel2:/work/intx033 ~/mnt/hummel_work'
 alias umount_hummelwork='fusermount -u ~/mnt/hummel_work'
 
+# syn - ws07
+alias mount_ws07home='sshfs -o idmap=user,umask=$(umask),gid=$(id -g),uid=$(id -u),user,reconnect,ServerAliveInterval=15 dsp@ws07.synergeticon.com:/home/dsp ~/mnt/syn_ws07_home'
+alias mount_ws07srv='sshfs -o idmap=user,umask=$(umask),gid=$(id -g),uid=$(id -u),user,reconnect,ServerAliveInterval=15 dsp@ws07.synergeticon.com:/srv ~/mnt/syn_ws07_srv'
+
 # ls aliases
 alias lsl='ls -lh'
 
 # virtual environments
-alias roboai36='source ~/roboai36/roboai36-venv/bin/activate'
+alias roboai36='source ~/roboai367/roboai367-venv/bin/activate'
+alias roboai-experimental='source ~/roboai-experimental/roboai367-venv/bin/activate'
 
 # feh aliases
 alias fehaa='feh --auto-zoom --force-alias'
 
-# RoboCup
-# A small collection of aliases and functions for our tools, enviroments and building our code
-## Locations
-export BITBOTS_CODE_DIR=~/repositories/robocup/code
-export BITBOTS_CHROOT_DIR=~/repositories/robocup/darwin-chroot
-
-## ChRoot
-#alias ChRoot="sudo mount --bind ~/Pictures/darwin-testimages ${BITBOTS_CHROOT_DIR}/home/darwin/Pictures && sudo ~/darwin-chroot/startup.sh ${BITBOTS_CODE_DIR} ; sudo umount ${BITBOTS_CHROOT_DIR}/home/darwin/Pictures"
-alias ChRoot="sudo ${BITBOTS_CHROOT_DIR}/startup.sh ${BITBOTS_CODE_DIR}"
-
-## Virtual env
-alias activate='source ${BITBOTS_CODE_DIR}/.py-env/bin/activate'
-alias reactivate='if [[ $VIRTUAL_ENV ]] ; then VENV="${VIRTUAL_ENV}" ; deactivate >/dev/null 2>&1 ; . ${VENV}/bin/activate; fi'
-### Interactive Python interpreter in Virtual env
-### The interactive python interpreter ipython and bpython didn't work fine inside an activated virtual-env, when not started directly inside, so here is a work around to start it inside if available
-alias ipython='$(executable ipython)'
-alias bpython='$(executable bpython)'
-
-## Tools
-alias GLRobot="activate ; simulation >/dev/null 2>&1 & glrobot >/dev/null 2>&1 &"
-alias rstat='export CUR=`pwd -L` && cd ${BITBOTS_CODE_DIR} && git status && cd $CUR'
-alias robocup_export="export DEBUG=1 && export VISION_DEBUG=1 && export VISION_DEBUG_FRAME_SKIP=0 && export DEBUG_HOST=127.0.0.1 && export DEBUG_LEVEL=5"
 alias gitroot='cd $(git rev-parse --show-toplevel)'
-
-## Build definements
-## Note these building aliases require the "correct" virtual-env location in the code folder, or at least a valid symbolic link.
-## Otherwise the virtual-env has to be activated.
-alias build="${BITBOTS_CODE_DIR}/build"
-alias Qbuild="QUIET=1 build"
-alias Rbuild="build Release"
-alias QRbuild="Qbuild Release"
-alias RWDbuild="build ReleaseWithDebInf"
-alias QRWDbuild="QUIET=1 RWDbuild"
-alias Cbuild="build --clean -e"
-alias CEbuild="build --clean-env -e"
-alias CBbuild="build --clean-build-path -e"
-
-## Debugger
-alias pygdb="gdb -args python"
-### consider the "bitbots_debug" function
-
-
-# Some defined functions to provide more features that it's possible with aliases
-## Start gdb for our python executables e.g. bitbots_debug simulation
-function bitbots_debug {
-    if ! [ $VIRTUAL_ENV ];then
-        activate
-    fi
-    pygdb $VIRTUAL_ENV/bin/$@
-}
 
 ## find executables of a given program line ipython that can be installed on the system and in the virtual-env
 function executables {
